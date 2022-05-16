67adeb684c7a60d5e61c33c21b535f6d82cb1b1f
JuanK81 <jcasalsbadolato@gmail.com>
2022-05-16 16:02:50 +0200

    add nested navigation

--- App.js
+++ App.js
@@ -2,12 +2,64 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import CategoriesScreen from './screens/CategoriesScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Messages" component={Messages} />
+    </Tab.Navigator>
+  );
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#12213d' },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: '#4173b4',
+        },
+        drawerContentStyle: { backgroundColor: '#12213d' },
+        drawerInactiveTintColor: '#fff',
+        drawerActiveTintColor: '#ffd000',
+        drawerActiveBackgroundColor: '#4173b4',
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name="list" />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          title: 'Favorites',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name="star" />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -24,32 +76,43 @@ export default function App() {
             },
           }}
         >
-          <Stack.Screen
+
+          {/* ************** */}
+
+          {/* <Stack.Screen
             name="MealsCategories"
             component={CategoriesScreen}
             options={{
-              title: 'All Categories',
+              title: 'All Categories Stak',
+            }}
+          /> */}
+
+          <Stack.Screen
+            name="DrawerCategories"
+            component={DrawerNavigator}
+            options={{
+              headerShown: false
             }}
           />
+
+          {/* ************** */}
+
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
           <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId
-            //   };
-            // }}
+            name="MealDetails"
+            component={MealDetailScreen}
+            options={{
+              headerRight: () => {
+                return <Button title="Tap Me!" />;
+              },
+              title: 'About the Meal'
+            }}
           />
-          <Stack.Screen name="MealDetails" component={MealDetailScreen} options={{
-            headerRight: () => {
-              return <Button title='Tap Me!'/>
-            },
-          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
+
 }
 
 const styles = StyleSheet.create({});
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const FavoritesScreen = () => {
+  return (
+    <View>
+      <Text>Favorites</Text>
+      <Text>Favorites</Text>
+      <Text>Favorites</Text>
+    </View>
+  );
+};
+
+export default FavoritesScreen;
